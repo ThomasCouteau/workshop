ef006a3ec1bf6f99899fa87e2357fa7d50b7ee70
ThomasCouteau <mrthomas.couteau@gmail.com>
2022-10-05 12:01:10 +0200

    card ajoutée

---
+++ src/components/CardBee.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="q-pa-md row items-center justify-center q-gutter-md">
+    <q-card class="my-card">
+      <img :src="img" />
+
+      <q-card-section>
+        <div class="text-h6">N° {{ num }}</div>
+        <div class="text-subtitle2">{{ author }}</div>
+      </q-card-section>
+
+      <q-card-section class="q-pt-none">
+        {{ description }}
+      </q-card-section>
+    </q-card>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "CardBee",
+  props: {
+    num: {
+      type: Number,
+      required: true,
+    },
+
+    author: {
+      type: String,
+      default: "Un propriètaire",
+    },
+
+    img: {
+      type: String,
+      required: true,
+    },
+
+    description: {
+      type: String,
+      default: "Une ruche connectée",
+    },
+  },
+});
+</script>
+
+<style lang="sass" scoped>
+.my-card
+  width: 100%
+  max-width: 500px
+</style>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,13 +1,51 @@
 <template>
   <q-page>
-    <h1>Accueil</h1>
+    <h4 class="text-h4 text-center">Liste des ruches connectées</h4>
+    <CardBee v-for="num in cardList" :key="num.title" v-bind="num" />
   </q-page>
 </template>
 
 <script>
-import { defineComponent } from "vue";
+import { defineComponent, ref } from "vue";
+import CardBee from "components/CardBee.vue";
+
+const beeCards = [
+  {
+    num: 1,
+    author: "home",
+    description: "",
+    img: "https://fr.jardins-animes.com/images/extraimages/ruche-abeille-inoffensive-jardin.jpg",
+  },
+  {
+    num: 2,
+    author: "home",
+    description: "",
+    img: "https://ruches-et-ruchettes-de-france.com/2-large_default/ruche-dadant-10c-plancher-bois-hausse-toit-chalet.jpg",
+  },
+  {
+    num: 3,
+    author: "home",
+    description: "",
+    img: "https://monjardinmamaison.maison-travaux.fr/wp-content/uploads/sites/8/2020/06/gettyimages-665690923-614x410.jpg",
+  },
+  {
+    num: 4,
+    author: "home",
+    description: "",
+    img: "https://www.rucherecole.fr/img/cms/ruches/Dadant/trou-de-vol-rond-ruche-Dadant.jpg",
+  },
+];
 
 export default defineComponent({
   name: "IndexPage",
+  components: {
+    CardBee,
+  },
+
+  setup() {
+    return {
+      cardList: beeCards,
+    };
+  },
 });
 </script>
